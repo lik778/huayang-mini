3db5fa8db00b81ea10909ab136c39422268a6fce
pengfei <dupengfei@baixing.com>
2020-04-17 17:46:35 +0800

    优化公众号加入花样大学的流程；

--- pages/auth/auth.js
+++ pages/auth/auth.js
@@ -1,10 +1,12 @@
 // pages/auth/auth.js
 import { wxGetUserInfoPromise } from '../../utils/auth.js'
 import { GLOBAL_KEY } from '../../lib/config.js'
-import { bindUserInfo, bindWxPhoneNumber } from "../../api/auth/index"
+import { bindUserInfo, bindWxPhoneNumber, getWxInfo } from "../../api/auth/index"
 import { getLocalStorage, setLocalStorage } from "../../utils/util"
 import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog'
 import { getUniversityCode } from "../../api/mine/index"
+import { APP_LET_ID } from "../../lib/config"
+import { wxLoginPromise } from "../../utils/auth"
 
 Page({
 
@@ -30,64 +32,73 @@ Page({
 	 */
 	getUserInfo() {
 		try {
-			wxGetUserInfoPromise().then(async (response) => {
-				const userInfo = response.userInfo
-				let params = {
-					open_id: getLocalStorage(GLOBAL_KEY.openId),
-					avatar_url: userInfo.avatarUrl,
-					city: userInfo.city,
-					nickname: userInfo.nickName,
-					province: userInfo.province,
-					country: userInfo.country,
-					gender: userInfo.gender
-				}
-				let originUserInfo = await bindUserInfo(params)
-				setLocalStorage(GLOBAL_KEY.userInfo, originUserInfo)
-				if (this.data.didFromArticle) {
-					let userId = getLocalStorage(GLOBAL_KEY.userId)
-					let { is_zhide_vip, student_num } = getLocalStorage(GLOBAL_KEY.accountInfo) ? JSON.parse(getLocalStorage(GLOBAL_KEY.accountInfo)) : {}
-					if (userId == null) {
-						this.setData({
-							show: true
-						})
-					} else {
-						// 1 是否是VIP
-						if (is_zhide_vip) {
-							if (student_num) {
-								Dialog.confirm({
-									title: '提示',
-									message: '您已是花样大学学员',
-									confirmButtonText: '查看课程',
-									showCancelButton: false
-								}).then(() => {
-									getUniversityCode(`user_key=daxue`).then(res => {
-										wx.navigateTo({
-											url: `/subLive/courseList/courseList?id=${res.data.id}`,
-										})
-									})
-								}).catch(() => {})
-							} else {
-								wx.navigateTo({
-									url: '/mine/joinSchool/joinSchool',
+			wxLoginPromise()
+				.then(async (code) => {
+					// 用code查询服务端是否有该用户信息，如果有更新本地用户信息，反之从微信获取用户信息保存到服务端
+					let wxOriginUserInfo = await getWxInfo({code, app_id: APP_LET_ID.tx})
+					// 缓存openId
+					setLocalStorage(GLOBAL_KEY.openId, wxOriginUserInfo.openid)
+
+					wxGetUserInfoPromise().then(async (response) => {
+						const userInfo = response.userInfo
+						let params = {
+							open_id: getLocalStorage(GLOBAL_KEY.openId),
+							avatar_url: userInfo.avatarUrl,
+							city: userInfo.city,
+							nickname: userInfo.nickName,
+							province: userInfo.province,
+							country: userInfo.country,
+							gender: userInfo.gender
+						}
+						let originUserInfo = await bindUserInfo(params)
+						setLocalStorage(GLOBAL_KEY.userInfo, originUserInfo)
+						if (this.data.didFromArticle) {
+							let userId = getLocalStorage(GLOBAL_KEY.userId)
+							let { is_zhide_vip, student_num } = getLocalStorage(GLOBAL_KEY.accountInfo) ? JSON.parse(getLocalStorage(GLOBAL_KEY.accountInfo)) : {}
+							if (userId == null) {
+								this.setData({
+									show: true
 								})
+							} else {
+								// 1 是否是VIP
+								if (is_zhide_vip) {
+									if (student_num) {
+										Dialog.confirm({
+											title: '提示',
+											message: '您已是花样大学学员',
+											confirmButtonText: '查看课程',
+											showCancelButton: false
+										}).then(() => {
+											getUniversityCode(`user_key=daxue`).then(res => {
+												wx.navigateTo({
+													url: `/subLive/courseList/courseList?id=${res.data.id}`,
+												})
+											})
+										}).catch(() => {})
+									} else {
+										wx.navigateTo({
+											url: '/mine/joinSchool/joinSchool',
+										})
+									}
+								} else {
+									Dialog.confirm({
+										title: '提示',
+										message: '花样大学为花样汇超级会员专属权益，您暂无权限申请',
+										confirmButtonText: '立即加入“花样汇”',
+										showCancelButton: false
+									}).then(() => {
+										wx.navigateTo({
+											url: '/mine/joinVip/joinVip?from=article',
+										})
+									}).catch(() => {})
+								}
 							}
 						} else {
-							Dialog.confirm({
-								title: '提示',
-								message: '花样大学为花样汇超级会员专属权益，您暂无权限申请',
-								confirmButtonText: '立即加入“花样汇”',
-								showCancelButton: false
-							}).then(() => {
-								wx.navigateTo({
-									url: '/mine/joinVip/joinVip?from=article',
-								})
-							}).catch(() => {})
+							wx.navigateBack()
 						}
-					}
-				} else {
-					wx.navigateBack()
-				}
-			})
+					})
+				})
+
 		} catch (error) {
 			console.log('用户取消微信授权')
 		}
