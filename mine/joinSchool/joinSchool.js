656a0e740fb70e5d6d83eeef84af9734b27bf87a
pengfei <dupengfei@baixing.com>
2020-04-22 15:56:51 +0800

    修复花样大学入学申请分享功能；

--- mine/joinSchool/joinSchool.js
+++ mine/joinSchool/joinSchool.js
@@ -121,7 +121,7 @@ Page({
   onShareAppMessage: function () {
     return {
 			title: '花样大学入学申请',
-			path: '/mine/joinSchool/joinSchool'
+			path: '/mine/joinSchool/joinSchool?from=article'
 		}
   }
 })
