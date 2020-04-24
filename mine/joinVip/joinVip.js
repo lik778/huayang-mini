051a6b57552779dadf5321e986cb550626420d8c
吴俊杰 <wujunjie@baixing.com>
2020-04-25 00:12:18 +0800

    删除注释

--- mine/joinVip/joinVip.js
+++ mine/joinVip/joinVip.js
@@ -170,23 +170,6 @@ Page({
             this.setData({
                 bgList:data
             })
-            // let arr = []
-            // let _this = this
-            // for (let i in data) {
-            //     wx.getImageInfo({
-            //         src: data[i],
-            //         success(res) {
-            //             let obj = {
-            //                 src: res.path,
-            //                 height: res.height
-            //             }
-            //             arr.push(obj)
-            //             _this.setData({
-            //                 bgList: arr
-            //             })
-            //         }
-            //     })
-            // }
         })
     },
     /**
