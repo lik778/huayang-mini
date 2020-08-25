47d4eb9e2c4586a29feb5dbbc6529b59f6f325b9
吴俊杰 <wujunjie@baixing.com>
2020-08-25 09:42:32 +0800

    新增打印

--- components/auth/auth.js
+++ components/auth/auth.js
@@ -1,7 +1,21 @@
-import { wxGetUserInfoPromise, wxLoginPromise } from "../../utils/auth"
-import { bindUserInfo, bindWxPhoneNumber, getWxInfo } from "../../api/auth/index"
-import { APP_LET_ID, GLOBAL_KEY } from "../../lib/config"
-import { $notNull, getLocalStorage, setLocalStorage } from "../../utils/util"
+import {
+  wxGetUserInfoPromise,
+  wxLoginPromise
+} from "../../utils/auth"
+import {
+  bindUserInfo,
+  bindWxPhoneNumber,
+  getWxInfo
+} from "../../api/auth/index"
+import {
+  APP_LET_ID,
+  GLOBAL_KEY
+} from "../../lib/config"
+import {
+  $notNull,
+  getLocalStorage,
+  setLocalStorage
+} from "../../utils/util"
 
 Component({
   /**
@@ -13,10 +27,14 @@ Component({
       value: false,
       observer(newVal) {
         if (newVal) {
-          this.setData({didVisible: newVal})
+          this.setData({
+            didVisible: newVal
+          })
         } else {
           let timer = setTimeout(() => {
-            this.setData({didVisible: newVal})
+            this.setData({
+              didVisible: newVal
+            })
             clearTimeout(timer)
           }, 200)
         }
@@ -44,7 +62,10 @@ Component({
         wxLoginPromise()
           .then(async (code) => {
             // 用code查询服务端是否有该用户信息，如果有更新本地用户信息，反之从微信获取用户信息保存到服务端
-            let wxOriginUserInfo = await getWxInfo({code, app_id: APP_LET_ID.tx})
+            let wxOriginUserInfo = await getWxInfo({
+              code,
+              app_id: APP_LET_ID.tx
+            })
             // 缓存openId
             setLocalStorage(GLOBAL_KEY.openId, wxOriginUserInfo.openid)
 
@@ -87,13 +108,17 @@ Component({
           iv,
         }
         if (encrypted_data && iv) {
+          console.log(getApp().globalData.invite_user_id)
           if (getApp().globalData.invite_user_id) {
-            params = {...params, invite_user_id: getApp().globalData.invite_user_id}
+            params = {
+              ...params,
+              invite_user_id: getApp().globalData.invite_user_id
+            }
           }
           let originAccountInfo = await bindWxPhoneNumber(params)
           setLocalStorage(GLOBAL_KEY.accountInfo, originAccountInfo)
         }
-      this.complete()
+        this.complete()
       } else {
         // 用户拒绝手机号授权
         this.cancel()
@@ -120,13 +145,18 @@ Component({
     checkLogin() {
       wxLoginPromise()
         .then(async (code) => {
-          let originUserInfo = await getWxInfo({code, app_id: APP_LET_ID.tx})
+          let originUserInfo = await getWxInfo({
+            code,
+            app_id: APP_LET_ID.tx
+          })
           if ($notNull(originUserInfo) && originUserInfo.nickname) {
             // 缓存openId、userInfo
             setLocalStorage(GLOBAL_KEY.openId, originUserInfo.openid)
             setLocalStorage(GLOBAL_KEY.userInfo, originUserInfo)
             // 用户已完成微信授权，引导用户手机号授权
-            this.setData({didGetPhoneNumber: true})
+            this.setData({
+              didGetPhoneNumber: true
+            })
           }
         })
         .catch((error) => {
