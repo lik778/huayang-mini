21773688b8eb6f0d8315342973302f9099577983
wujunjie <wujunjie@baixing.com>
2021-01-27 15:16:12 +0800

    生成合作资源包页面

--- subCourse/cooperationPage/cooperationPage.js
+++ subCourse/cooperationPage/cooperationPage.js
@@ -1,4 +1,13 @@
 // subCourse/cooperationPage/cooperation.js
+import {
+  getCooperationById
+} from "../../api/course/index"
+import {
+  GLOBAL_KEY
+} from "../../lib/config"
+import {
+  getLocalStorage
+} from "../../utils/util"
 Page({
 
   /**
@@ -12,6 +21,43 @@ Page({
     }], //banner图列表
     currentBannerIndex: 0, //banner下标
     logoStyle: 0, //logo样式，0代表横版，1代表竖版
+    allData: '', //课程包信息
+    didShowAuth: false, //显示授权弹窗
+    authType: '', //0授完权跳往课程详情1跳往学院作业秀
+    currentGetVideoId: ''
+  },
+
+  // 取消授权
+  authCancelEvent() {
+    this.setData({
+      didShowAuth: false
+    })
+  },
+
+  // 确认授权
+  authCompleteEvent(e) {
+    this.setData({
+      didShowAuth: false
+    })
+    if (this.data.authType === 0) {
+      wx.navigateTo({
+        url: `/subCourse/videoCourse/videoCourse?videoId=${this.data.currentGetVideoId}`,
+      })
+      this.setData({
+        currentGetVideoId: '',
+        authType: ''
+      })
+    } else {
+      this.setData({
+        currentGetVideoId: '',
+        authType: ''
+      })
+      let colleage = this.data.allData.collaborate.work_belong
+      colleage = colleage === "fashion" ? 2 : colleage === "fitness" ? 1 : 3
+      wx.navigateTo({
+        url: `/subCourse/themeTask/themeTask?kecheng_type=1&kecheng_id=${colleage}`,
+      })
+    }
   },
 
   // 判断logo是横版还是竖版
@@ -33,10 +79,74 @@ Page({
     })
   },
 
+  // 返回首页
+  toIndex() {
+    wx.switchTab({
+      url: '/pages/discovery/discovery',
+    })
+  },
+
+  // 跳往课程详情
+  toCourseDetail(e) {
+    let courseData = e.currentTarget.dataset.item
+    let userInfo = getLocalStorage(GLOBAL_KEY.accountInfo)
+    if (!userInfo) {
+      this.setData({
+        authType: 0,
+        didShowAuth: true,
+        currentGetVideoId: courseData.id
+      })
+      return
+    }
+    wx.navigateTo({
+      url: `/subCourse/videoCourse/videoCourse?videoId=${courseData.id}`,
+    })
+  },
+
+  // 发布课程作业
+  toCollegeIndex() {
+    let userInfo = getLocalStorage(GLOBAL_KEY.accountInfo)
+    if (!userInfo) {
+      this.setData({
+        authType: 1,
+        didShowAuth: true
+      })
+      return
+    }
+    let colleage = this.data.allData.collaborate.work_belong
+    colleage = colleage === "fashion" ? 2 : colleage === "fitness" ? 1 : 3
+    wx.navigateTo({
+      url: `/subCourse/themeTask/themeTask?kecheng_type=1&kecheng_id=${colleage}`,
+    })
+  },
+
   // 切换banner下标
   changeCurrentBannerIndex(e) {
     this.setData({
       currentBannerIndex: e.detail.current
+    })
+  },
+
+  // 获取合作包信息
+  getData(id) {
+    getCooperationById({
+      snow_id: id
+    }).then(res => {
+      if (res.code === 0) {
+        console.log(res.data)
+        let list = res.data.kecheng_list
+        if (list.length > 0) {
+          list.map(item => {
+            if (item.visit_count >= 10000) {
+              item.visit_count = (item.visit_count / 10000).toFixed(1) + "万"
+              item.visit_count = item.visit_count.split('.')[1] === '0万' ? item.visit_count[0] + "万" : item.visit_count
+            }
+          })
+        }
+        this.setData({
+          allData: res.data
+        })
+      }
     })
   },
 
@@ -54,6 +164,7 @@ Page({
       return
     }
     console.log('packageId=' + packageId)
+    this.getData(packageId)
     this.checkLogoStyle('https://pic1.zhimg.com/v2-5b5a2fa02cb65f2ab1910439fec5791f_l.jpg')
   },
 
