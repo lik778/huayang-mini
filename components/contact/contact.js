e0402a3302606e2af54ae2a1dce7e4997af6f5fd
wujunjie <wujunjie@baixing.com>
2021-02-20 16:27:37 +0800

    客服消息适配

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -1,4 +1,13 @@
 // components/contact/contact.js
+import {
+  getScene
+} from "../../api/mine/index"
+import {
+  GLOBAL_KEY
+} from "../../lib/config"
+import {
+  getLocalStorage
+} from "../../utils/util"
 Component({
   /**
    * 组件的属性列表
@@ -8,6 +17,10 @@ Component({
       value: "https://huayang-img.oss-cn-shanghai.aliyuncs.com/1613793879bWSJjA.jpg",
       type: String
     }, //猜你要发图片地址
+    isRootPAge: {
+      value: false,
+      type: Boolean
+    }, //是否是tab页,如果是tab页距离底部高度需要兼容tab高度
     showMessageCard: {
       value: true,
       type: Boolean
@@ -17,6 +30,7 @@ Component({
       type: String
     }, //引导按钮名称
     sendMessageTitle: {
+      value: "请点击下方二维码，长按扫码加专属客服",
       type: String
     }, //猜你要发发送卡片标题
     sessionFrom: {
@@ -39,6 +53,13 @@ Component({
     // 关闭
     close() {
       this.triggerEvent('closeContactModal', true)
+    },
+    // 设置场景值
+    setScene() {
+      getScene({
+        open_id: getLocalStorage(GLOBAL_KEY.openId),
+        scene: 'distribute'
+      })
     }
   }
 })
