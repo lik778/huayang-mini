5dd18b8162393367beea345f5721fa00654dfe60
hengjie <965450326@qq.com>
2021-04-21 10:51:40 +0800

    chj_dev college update:share layout .esc

--- subCourse/freeOnlineCourse/freeOnlineCourse.js
+++ subCourse/freeOnlineCourse/freeOnlineCourse.js
@@ -1,57 +1,59 @@
-
 import {
     getFreeOnlineCourse
 } from "../../api/course/index";
 
 Page({
-    data:{
-        pagesControl:{
-            limit:10,
-            offset:0
+    data: {
+        pagesControl: {
+            limit: 10,
+            offset: 0
         },
-        videoList:[
-        ]
+        videoList: []
     },
-    onLoad(){
+    onLoad() {
+        wx.showShareMenu({
+            withShareTicket: true,
+            menus: ['shareAppMessage', 'shareTimeline']
+        })
         this.getvideoList();
     },
-    onReachBottom(){
+    onReachBottom() {
         this.getvideoList();
     },
-    toCourseDetail(e){
+    toCourseDetail(e) {
         let detail = e.currentTarget.dataset.video_detail;
         console.log(detail);
     },
-    
+
     /**
-	 * 跳转到视频详情页
-	 * @param e
-	 */
-	goToVideoDetail(e) {
-		let {
-			id,
-			name,
-			desc
-		} = e.currentTarget.dataset.item;
-		wx.navigateTo({
-			url: `/subCourse/videoCourse/videoCourse?videoId=${id}`,
-		})
-	},
-    getvideoList(){
+     * 跳转到视频详情页
+     * @param e
+     */
+    goToVideoDetail(e) {
+        let {
+            id,
+            name,
+            desc
+        } = e.currentTarget.dataset.item;
+        wx.navigateTo({
+            url: `/subCourse/videoCourse/videoCourse?videoId=${id}`,
+        })
+    },
+    getvideoList() {
         let params = {
-            limit:this.data.pagesControl.limit,
-            offset:this.data.pagesControl.offset
+            limit: this.data.pagesControl.limit,
+            offset: this.data.pagesControl.offset
         };
-        getFreeOnlineCourse(params).then(list=>{
+        getFreeOnlineCourse(params).then(list => {
             console.log(list)
             this.setData({
-                videoList:this.data.videoList.concat(list),
-                pagesControl:{
-                    limit:this.data.pagesControl.limit,
-                    offset:this.data.pagesControl.limit+this.data.pagesControl.offset,
+                videoList: this.data.videoList.concat(list),
+                pagesControl: {
+                    limit: this.data.pagesControl.limit,
+                    offset: this.data.pagesControl.limit + this.data.pagesControl.offset,
                 }
             })
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err);
         })
     },
