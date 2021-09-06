bd07555b38b1938c33e9ff5116fbcb75dcbef94d
wujunjie <wujunjie@baixing.com>
2021-09-06 15:53:05 +0800

    完善打点

--- components/videoSwiper/video-swiper.js
+++ components/videoSwiper/video-swiper.js
@@ -30,7 +30,8 @@ Component({
     swiperDotIsEnd: false,
     animationingStatus: false,
     swiperPlayDuration: '',
-    videoLoading: true
+    videoLoading: true,
+    videoTotalDuration: ''
   },
 
   lifetimes: {
@@ -55,7 +56,8 @@ Component({
         /* 头部视频内容主动滑动打点 */
         bxPoint("new_homepage_header_vedio_slide", {
           header_vedio_id: this.data.videoList[Number(e.detail.currentItemId) - 1].id,
-          header_vedio_play_duration: this.data.swiperPlayDuration
+          header_vedio_play_duration: this.data.swiperPlayDuration,
+          header_vedio_duration: this.data.videoTotalDuration
         }, false)
       } else {
         /* 右滑 */
@@ -63,7 +65,8 @@ Component({
         /* 头部视频内容主动滑动打点 */
         bxPoint("new_homepage_header_vedio_slide", {
           header_vedio_id: this.data.videoList[Number(e.detail.currentItemId) + 1].id,
-          header_vedio_play_duration: this.data.swiperPlayDuration
+          header_vedio_play_duration: this.data.swiperPlayDuration,
+          header_vedio_duration: this.data.videoTotalDuration
         }, false)
       }
 
@@ -74,7 +77,8 @@ Component({
       }
 
       this.setData({
-        currentVideoIndex: index
+        currentVideoIndex: index,
+        videoTotalDuration: ''
       })
       this.playVideoCommon(index)
     },
@@ -84,6 +88,7 @@ Component({
       this.setData({
         swiperPlayDuration: Math.round(e.detail.currentTime),
         videoLoading: false,
+        videoTotalDuration: parseInt(e.detail.duration)
       })
     },
 
