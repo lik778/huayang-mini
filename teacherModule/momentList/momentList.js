d1aa098c9610f7ec11d5ceaa45395d78ab0263b0
wujunjie <wujunjie@baixing.com>
2021-12-31 12:12:59 +0800

    修复若干问题

--- teacherModule/momentList/momentList.js
+++ teacherModule/momentList/momentList.js
@@ -133,7 +133,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
     if (options.teacherId && options.teacherUserId) {
       this.setData({
         ['pagination.tutor_id']: options.teacherId,
