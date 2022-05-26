9db4ed47753e0b2d7a6a2833daf9a4c355ca8d85
wujunjie <wujunjie@baixing.com>
2022-05-26 17:10:55 +0800

    报名结果页优化

--- utils/bxPoint.js
+++ utils/bxPoint.js
@@ -76,7 +76,7 @@ const bxPoint = function(siteId, params, ispv = true) {
     ...params,
     time: +new Date(),
     site_id: siteId,
-    // __debug: "1", // 打点数据，测试环境携带__dev，上线后去除该字段
+    __debug: "1", // 打点数据，测试环境携带__dev，上线后去除该字段
     tracktype: ispv ? "pageview" : "event",
     event_type: 'huayang'
   }
